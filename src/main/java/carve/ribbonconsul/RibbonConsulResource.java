dc9a4871e33fb97c8fe0cc519350417246daaee8
rikcarve <rik@carve.ch>
2016-11-30 20:57:47 +0100

    add MDC to request header

--- src/main/java/carve/ribbonconsul/RibbonConsulResource.java
+++ src/main/java/carve/ribbonconsul/RibbonConsulResource.java
@@ -3,16 +3,18 @@ package carve.ribbonconsul;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 
-import javax.annotation.PostConstruct;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+
+import org.slf4j.MDC;
 
 import com.fasterxml.jackson.core.JsonParseException;
 import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.netflix.ribbon.ClientOptions;
 import com.netflix.ribbon.Ribbon;
+import com.netflix.ribbon.http.HttpRequestBuilder;
 import com.netflix.ribbon.http.HttpRequestTemplate;
 import com.netflix.ribbon.http.HttpResourceGroup;
 
@@ -21,7 +23,7 @@ import io.netty.buffer.ByteBuf;
 @Path("/ribbon")
 public class RibbonConsulResource {
 
-    // static HelloService helloProxy = Ribbon.from(HelloService.class);
+    private ObjectMapper mapper = new ObjectMapper();
 
     static {
         System.setProperty("ribbon.NIWSServerListClassName", "carve.ribbonconsul.ConsulServerList");
@@ -48,17 +50,11 @@ public class RibbonConsulResource {
             .withUriTemplate("/hello/v1/hello/world")
             .build();
 
-    @PostConstruct
-    public void init() {
-        // helloProxy = Ribbon.from(HelloService.class);
-    }
-
     @GET
     @Path("hello")
     @Produces("text/plain")
     public String hello() {
         ByteBuf resp = helloTemplate.requestBuilder().build().execute();
-        // ByteBuf resp = helloProxy.sayWorld().execute();
         return resp.toString(StandardCharsets.ISO_8859_1);
     }
 
@@ -66,9 +62,14 @@ public class RibbonConsulResource {
     @Path("world")
     @Produces("text/plain")
     public String world() throws JsonParseException, JsonMappingException, IOException {
-        ByteBuf resp = worldTemplate.requestBuilder().build().execute();
-        ObjectMapper mapper = new ObjectMapper();
+        MDC.put("REQUEST-ID", "12345678");
+        ByteBuf resp = requestBuilderWithHeaders(worldTemplate).build().execute();
         return mapper.readValue(resp.toString(StandardCharsets.ISO_8859_1), World.class).getHelloWorld();
     }
 
+    public static HttpRequestBuilder<ByteBuf> requestBuilderWithHeaders(HttpRequestTemplate<ByteBuf> template) {
+        HttpRequestBuilder<ByteBuf> builder = template.requestBuilder();
+        MDC.getCopyOfContextMap().forEach((k, v) -> builder.withHeader("X-MDC-" + k, v));
+        return builder;
+    }
 }
